177e74cdf987b98f2cd478df39733deca6889f25
shdjsh <106967270+shdjsh@users.noreply.github.com>
2024-04-13 15:08:53 +0800

    pull commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("master test");
         System.out.println("got-fix test");
+        System.out.println("push test");
         System.out.println("pull test");
     }
 }
